9f64d6f6164b45b5d70367e8c23d5758df259dc9
Vincent Ramdhanie <vramdhanie@gmail.com>
2021-01-16 14:31:50 -0400

    The reverse setup files

---
+++ src/reverse/readme.md
@@ -0,0 +1,13 @@
+# Reverse a list
+
+Write an algorithm to reverse a linked list. That is, given a linked list such as:
+
+![A linked list](reverse_001.png)
+
+your algorithm should return the linked list:
+
+![Reversed linked list](reverse_002.png)
+
+The time complexity of your algorithm should be linear (O(n)).
+
+Attempt to solve this problem using both recursive and iterative algorithms.
---
+++ src/test/reverse.test.js
@@ -0,0 +1,50 @@
+const { reverseIterative, reverseRecursive } = require("../reverse/reverse");
+const LinkedList = require("../lib/linkedList");
+
+describe("reverse a list", () => {
+  describe("Iterative algorithm", () => {
+    it("should return empty list for empty list", () => {
+      const list = new LinkedList();
+      const expected = new LinkedList();
+      const actual = reverseIterative(list);
+      expect(actual).toEqual(expected);
+    });
+
+    it("should return the same list if just one element", () => {
+      const list = new LinkedList([1]);
+      const expected = new LinkedList([1]);
+      const actual = reverseIterative(list);
+      expect(actual).toEqual(expected);
+    });
+
+    it("should reverse a list of numbers", () => {
+      const list = new LinkedList([1, 2, 3, 4, 5]);
+      const expected = new LinkedList([5, 4, 3, 2, 1]);
+      const actual = reverseIterative(list);
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe("Recursive algorithm", () => {
+    it("should return empty list for empty list", () => {
+      const list = new LinkedList();
+      const expected = new LinkedList();
+      const actual = reverseRecursive(list);
+      expect(actual).toEqual(expected);
+    });
+
+    it("should return the same list if just one element", () => {
+      const list = new LinkedList([1]);
+      const expected = new LinkedList([1]);
+      const actual = reverseRecursive(list);
+      expect(actual).toEqual(expected);
+    });
+
+    it("should reverse a list of numbers", () => {
+      const list = new LinkedList([1, 2, 3, 4, 5]);
+      const expected = new LinkedList([5, 4, 3, 2, 1]);
+      const actual = reverseRecursive(list);
+      expect(actual).toEqual(expected);
+    });
+  });
+});
