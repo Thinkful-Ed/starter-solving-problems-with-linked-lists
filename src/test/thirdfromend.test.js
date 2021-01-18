a2d81f002a27fda697fc8dfc735936849d02a698
Vincent Ramdhanie <vramdhanie@gmail.com>
2021-01-18 15:44:48 -0400

    Setup for the third from end problem

---
+++ src/test/thirdfromend.test.js
@@ -0,0 +1,30 @@
+const thirdFromEnd = require("../thirdfromend/thirdfromend");
+const LinkedList = require("../lib/linkedList");
+
+describe("Third From End", () => {
+  it("should return null for empty list", () => {
+    const list = new LinkedList();
+    const actual = thirdFromEnd(list);
+    expect(actual).toBeNull();
+  });
+
+  it("should return null for list length < 3", () => {
+    const list = new LinkedList([1, 2]);
+    const actual = thirdFromEnd(list);
+    expect(actual).toBeNull();
+  });
+
+  it("should return head for list length == 3", () => {
+    const list = new LinkedList([1, 2, 3]);
+    const actual = thirdFromEnd(list);
+    expect(actual).not.toBeNull();
+    expect(actual.value).toEqual(1);
+  });
+
+  it("should return third from last for longer lists", () => {
+    const list = new LinkedList([1, 2, 3, 4, 5]);
+    const actual = thirdFromEnd(list);
+    expect(actual).not.toBeNull();
+    expect(actual.value).toEqual(3);
+  });
+});
