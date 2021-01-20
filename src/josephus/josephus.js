1af6f421c7cc91997c38cd3becfda17e9c2f1404
Vincent Ramdhanie <vramdhanie@gmail.com>
2021-01-20 13:51:59 -0400

    Solution for Josephus problem

--- src/josephus/josephus.js
+++ src/josephus/josephus.js
@@ -1,3 +1,18 @@
-function josephus(list, m) {}
+function josephus(list, m) {
+  let node = list.head;
+  const tail = list.find((node) => !node.next);
+  tail.next = list.head;
+  while (node.next != node) {
+    for (let i = 1; i < m; i++) {
+      node = node.next;
+    }
+    if (node.next === list.head) {
+      list.head = list.head.next;
+    }
+    node.next = node.next.next;
+    node = node.next;
+  }
+  return node.value;
+}
 
 module.exports = josephus;
