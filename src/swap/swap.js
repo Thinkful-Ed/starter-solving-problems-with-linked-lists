7dcf8b59141b6cafbad9543fdc9db9d6b7b0c8de
Vincent Ramdhanie <vramdhanie@gmail.com>
2021-01-20 10:24:28 -0400

    Solution for Swap Nodes Problem

--- src/swap/swap.js
+++ src/swap/swap.js
@@ -4,6 +4,30 @@
  * @param {Node} x
  * @param {Node} y
  */
-function swap(list, x, y) {}
+function swap(list, x, y) {
+  if (!list.head) {
+    return list;
+  }
+
+  const x_next = x.next;
+  const x_prev = list.findWithPrevious((node) => node === x)[1];
+  const y_prev = list.findWithPrevious((node) => node === y)[1];
+
+  x.next = y.next;
+  y.next = x_next;
+  if (x_prev) {
+    x_prev.next = y;
+  } else {
+    list.head = y;
+  }
+
+  if (y_prev) {
+    y_prev.next = x;
+  } else {
+    list.head = x;
+  }
+
+  return list;
+}
 
 module.exports = swap;
